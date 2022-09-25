77ccc53c52b5b090e87435b2536f60d2db35cc49
naha0 <780400335@qq.com>
2022-09-25 14:49:17 +0800

    tab-nav完成，home页面完成小部分

---
+++ src/components/nav-bar/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="nav-bar">
+    <div class="left">
+      <slot name="left"></slot>
+    </div>
+
+    <div class="middle">
+      <slot name="middle"></slot>
+    </div>
+
+    <div class="right">
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="less" scoped>
+.nav-bar {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 0;
+  .left {
+    margin-left: 20px;
+  }
+  .middle {
+    font-size: 16px;
+    font-weight: 600;
+  }
+  .right {
+    margin-right: 20px;
+  }
+}
+</style>
